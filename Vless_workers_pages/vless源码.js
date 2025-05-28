e4f418f646d75e06faa6015481edacd3fc3803e4
fzpy666 <fzpy666@gmail.com>
2025-05-28 15:58:37 +0800

    Update vless源码.js

--- Vless_workers_pages/vless源码.js
+++ Vless_workers_pages/vless源码.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "62acd019-22da-4f41-b8f7-6feda2bf63bc";
 
 const proxyIPs = ["ts.hpc.tw"];
 const cn_hostnames = [''];
